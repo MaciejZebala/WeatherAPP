ef1cb0223a22e99ac5e84da420825ef4140a84ed
MaciejZebala <kropka551@op.pl>
2020-10-07 11:44:11 +0200

    Add navigation and Add scroll bar on mobile device

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './styles/index.scss'
 
 import Slider from './js/Slider';
 
+import Navigation from './js/Navigation'
+
 const slider = new Slider();
+
+const nav = new Navigation();
+
+nav.changeRing();
 
 slider.render();
 
