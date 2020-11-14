c197dca0e18708f3db8b14d6b2276f97885c5e1a
MaciejZebala <kropka551@op.pl>
2020-11-14 13:41:38 +0100

    Delete tomorrow button from navigation

--- src/js/Navigation.js
+++ src/js/Navigation.js
@@ -1,19 +1,14 @@
 export default class Navigation {
   constructor() {
-    this.tomorrow = document.querySelector('.nav__item--tomorrow');
     this.sevenDays = document.querySelector('.nav__item--seven-days');
     this.today = document.querySelector('.nav__item--today');
     this.ring = document.querySelector('.nav__ring');
   }
 
   changeRing() {
-    this.tomorrow.addEventListener('click', () => {
-      this.ring.style.transition = 'transform 0.3s ease-in-out';
-      this.ring.style.transform = 'translateX(109px)';
-    });
     this.sevenDays.addEventListener('click', () => {
       this.ring.style.transition = 'transform 0.3s ease-in-out';
-      this.ring.style.transform = 'translateX(240px)';
+      this.ring.style.transform = 'translateX(180px)';
     });
     this.today.addEventListener('click', () => {
       this.ring.style.transition = 'transform 0.3s ease-in-out';
